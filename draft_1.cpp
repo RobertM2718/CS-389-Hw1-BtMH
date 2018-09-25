6e6d8e69906af101253c371581b8a3493d80cc39
Robert W. McCaull <robertmccaull@rockisland.com>
2018-09-24 17:58:18 -0700

    First commit - nothing to report.

---
+++ draft_1.cpp
@@ -0,0 +1,50 @@
+/* This is the first-draft version of the code that Betsy and I will use to complete hw1.  
+*/
+
+
+#include <cassert>
+#include <chrono>
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std; //to make things simpler to write
+using namespace std::chrono;
+
+
+//const unsigned iterations0 = 32;
+//const unsigned iterations1 = iterations0*2;
+//const unsigned iterations2 = iterations1*2;
+//const unsigned iterations3 = iterations2*2;
+//const unsigned iterations4 = iterations3*2;
+//const unsigned iterations5 = iterations4*2;
+//hopefully, by making the size of this loop fixed at compile-time, we can get the compiler to remove the loop
+// from our compiled code, so that we're only measuring reads.  
+
+
+// read in column-major form from a 2d array of size (i, j)
+/*
+double time_read_it0() {
+	mat = malloc ()
+	const auto start = steady_clock::now();
+	for (unsigned i = 0; i < iterations0; i++) {
+		//do access
+	}
+	const auto end = steady_clock_now();
+}
+*/
+
+// takes a number of random indices.  Doesn't matter how slow
+unsigned[] create_indices (n) {
+	to_fill = 
+}
+
+duration<double> time_to_read(x, y, it) {
+
+}
+
+int main (int argc, char *argv[]) {
+	
+	malloc 
+	
+	free
+}
